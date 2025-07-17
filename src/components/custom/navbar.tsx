89af74ad157f686095a82da25cfd28280f241c70
hassan-31x <muhammadhassanchannel786@gmail.com>
2025-07-17 14:07:14 +0500

    added resume download option

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -72,9 +72,19 @@ export const Navbar = () => {
             <TooltipProvider>
               <Tooltip>
                 <TooltipTrigger asChild>
-                  <Button variant="outline" size="icon" className="outline-none shadow-xs border-[1px] rounded-md size-9 bg-white dark:bg-neutral-900 border-neutral-200 dark:border-neutral-700">
-                    <IconDownload className="size-4 text-neutral-500 dark:text-neutral-400" />
-                  </Button>
+                  <a
+                    href="/resume.pdf"
+                    download="resume.pdf"
+                    tabIndex={-1}
+                  >
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="outline-none shadow-xs border-[1px] rounded-md size-9 bg-white dark:bg-neutral-900 border-neutral-200 dark:border-neutral-700"
+                    >
+                      <IconDownload className="size-4 text-neutral-500 dark:text-neutral-400" />
+                    </Button>
+                  </a>
                 </TooltipTrigger>
                 <TooltipContent>
                   <p>Download Resume</p>
